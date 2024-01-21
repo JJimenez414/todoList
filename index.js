59eb7ef1889df764b0978ada1d77c83b383c1cf5
Jose <joseangel130414@gmail.com>
2024-01-21 00:18:30 -0600

    FIFTH COMMIT - Started on database. Start on user input and saving it into the db.

--- index.js
+++ index.js
@@ -2,22 +2,47 @@ import express from "express";
 import {dirname} from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import pg from "pg";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
+//body parser is a middleware which is used to communicate between two applications. 
 app.use(bodyParser.urlencoded({extended:true}));
+//telling express where to get static files
 app.use(express.static(__dirname + "/public"));
+//connecting to db in from pgadmin
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "kakis",
+    password: "id130414",
+    port: 5432
+});
+
+db.connect();
+
+//get the list that you want and save it in result
+const result = await db.query("SELECT item FROM list");
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-    console.log("hello");
+    //a list that we will store on the items that the user enters
+    const arrItems = [];
+    //filter the items from the list and save it int the arrItems
+    result.rows.forEach((items) => {
+        arrItems.push(items.item);
+    });
+
+
+    res.render("index.ejs", {items: dicItems});
 })
 
-// app.get("/newItem", (req, res) => {
-//     console.log("new tab");
-// });
 
 app.post("/newItem", (req, res) => {
+   /*
+   we are rendering index.ejs and we are passing the variable TITLE to it with the information
+   gathered by the reques made by item in 
+  */  
+    res.render("index.ejs", {title: req.body["item"]})
     console.log(req.body);
 });
 
@@ -64,3 +89,8 @@ app.listen(3000, () => {
 
 // });
 
+
+//NOTES: 
+// - node.js and express are used to create a server
+// - EJS is to create templates
+//  - EJS we need a folder called public for all static files such as img and styling (CSS)

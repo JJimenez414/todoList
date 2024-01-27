66ba8edaa0ed90395f8e0a4c019d8b820ee9aabf
Jose <joseangel130414@gmail.com>
2024-01-27 00:53:14 -0600

    SIXTH COMMIT - Fixed some of the css. Now im tyring to get the post fixed for checked boxes. Afterwards im going to work on implementing jquery or React.js

--- index.js
+++ index.js
@@ -58,51 +58,10 @@ app.post("/newItem", (req, res) => {
     res.redirect("/");
 });
 
+app.post("/deleteItem", (req, res) => {
+    console.log("hello");
+});
+
 app.listen(3000, () => {
     console.log("Server running...");
 });
-
-// //add item to the list
-// $("#btnEnter").click(function() {
-//     const newItem = $("#newItem").val();
-
-//     if(newItem != "") {
-//         $("ul").append($("<li></li>").append(newItem).addClass("listItem hoverClass"));
-
-//         $("#newItem").val("");
-//     } else {
-//         alert("Invalid Input");
-//     }
-// });
-
-// //if item is clicked we apply the checked class
-// $(document).on("click", ".listItem", function() {
-//     if($(this).hasClass("checked")) {
-//         $(this).removeClass("checked");
-//     } else {
-//         $(this).addClass("checked");
-//     }
-// });
-
-// //label section - make it for it to appear when label clicked.
-// $(".alertLabel").click(function() {
-
-//     //reference: https://stackoverflow.com/questions/27143063/jquery-how-to-add-class-to-the-child-element
-//     //closest: findes the closest parent div to the element.
-//     //find: finds the elements that have the class .alertTodo.
-//     //addClass: adds the clas hide Items to the element. 
-
-//     if($(this).closest("div").find(".alertTodo").hasClass("hideItems"))
-//      {
-//         $(this).closest("div").find(".alertTodo").removeClass("hideItems");
-//      } else {
-//         $(this).closest("div").find(".alertTodo").addClass("hideItems");
-//      }
-
-// });
-
-
-//NOTES: 
-// - node.js and express are used to create a server
-// - EJS is to create templates
-// - EJS we need a folder called public for all static files such as img and styling (CSS)
